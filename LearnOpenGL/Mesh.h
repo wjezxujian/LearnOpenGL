45f1bb01ec93d64f63d26d54bbca0ecd29a3358a
jian.xu <504581303@qq.com>
2018-01-19 22:13:47 +0800

    update Assimp

--- LearnOpenGL/Mesh.h
+++ LearnOpenGL/Mesh.h
@@ -19,6 +19,7 @@ struct Texture
 {
 	unsigned int id;
 	std::string type;
+	std::string path;
 };
 
 class Mesh
---
+++ LearnOpenGL/Model.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Shader.h"
+#include "Mesh.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <vector>
+
+class Model
+{
+public:
+	Model(char* path)
+	{
+		loadModel(path);
+	}
+	void Draw(Shader shader);
+
+private:
+	void loadModel(std::string path);
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+
+private:
+	std::vector<Mesh> meshes;
+	std::string directory;
+	std::vector<Texture> textures_loaded;
+
+};
